1a1cc026ee47fc73fbf43ba4eca915354cfcd2f6
Gregg Whitworth <whitwort@gmail.com>
2017-07-12 16:25:32 -0400

    switch from CPP to rust

---
+++ src/rustlib/src/lib.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern  crate rustr;
+pub mod export;
+pub use rustr::*;
+
+fn max(x: &[u64]) -> u64 {
+    *x.iter().max_by(|x, y| x.cmp(y)).unwrap()
+}
+
+fn all_true(x: &Vec<bool>) -> bool {
+    for i in 0..x.len() {
+        if x[i] == false { return false; }
+    }
+
+    true
+}
+
+// #[rustr_export]
+pub fn vec_check(a: &Vec<u64>, b: &Vec<u64>) -> bool {
+
+    let max_n  = max(b) - 1;
+    let mut ns = vec![false; (max_n + 1) as usize];
+
+    for (index, residual) in a.iter().enumerate() {
+        let modulo = b[index];
+
+        let mut i = 0;
+        loop {
+            let n = (i * modulo) + residual;
+            if n > max_n { break; }
+            ns[n as usize] = true;
+            i += 1;
+        }
+
+    }
+
+    all_true(&ns)
+}
---
+++ src/rustlib/target/release/librustlib.d
@@ -0,0 +1,1 @@
+/home/gregg/coveR/src/rustlib/target/release/librustlib.rlib: /home/gregg/coveR/src/rustlib/src/lib.rs /home/gregg/coveR/src/rustlib/src/export.rs
