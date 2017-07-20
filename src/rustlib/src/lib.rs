f5ef1132fd6a8209605ffb6ddb5161c6d595eef6
jenningsa19 <jenningsa19@mail.wlu.edu>
2017-07-20 12:10:33 -0400

    Update lib.rs

--- src/rustlib/src/lib.rs
+++ src/rustlib/src/lib.rs
@@ -54,7 +54,7 @@ pub fn check_covering(r: &Vec<u64>, m: &Vec<u64>) -> bool {
     all_true(&ns)
 }
 
-pub fn vectorize(size: i32) -> Vec<i32> {
+pub fn vectorize(size: i32) -> Vec<u32> {
     let mut zero_vec: Vec<u32> = Vec::with_capacity(size);
     for num in 0..size {
         zero_vec.push(num);
